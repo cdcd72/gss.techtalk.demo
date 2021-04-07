4561b9ec32d72aebfefd66fa478010a0527721fc
cdcd72 <cdcd71517@gmail.com>
2021-04-08 00:55:55 +0800

    Modify dotnet 5 initAccessor demo (add scenario example)

--- src/Dotnet5/InitAccessor.Demo/InitAccessor.Demo/Program.cs
+++ src/Dotnet5/InitAccessor.Demo/InitAccessor.Demo/Program.cs
@@ -35,6 +35,88 @@ namespace InitAccessor.Demo
 
             // 沒辦法修改 p1，因為 init 存取子關係...
             //p1.FirstName = "Pekora";
+
+            // ==============================================================================================
+
+            // --- C# 9 "之前"不變性的可能做法 ---
+
+            // 使用步驟 1 來初始化
+            // var x = new X() { Y = "Yee" }; // 可以使用 Object Initializer 來初始化物件，但直接失去不變性(很難預防)...
+            // Console.WriteLine();           // Yee
+            // x.Y = "Haha";
+            // Console.WriteLine();           // Haha
+
+            // 使用步驟 3 來初始化
+            // var x = new X() { Y = "Yee" }; // 沒辦法使用 Object Initializer 來初始化物件...
+            // var x = new X("Yee");          // 只能用 Constructor 來初始化物件...
+            // x.Y = "Haha";                  // CS0200 error
+
+            // --- C# 9 "之後"不變性的可能做法 ---
+
+            // 使用步驟 4 來初始化
+            // var x = new X() { Y = "Yee" }; // 沒辦法使用 Object Initializer 來初始化物件...
+            // var x = new X("Yee");          // 只能用 Constructor 來初始化物件...
+            // x.Y = "Haha";                  // CS8852 error
+
+            // 使用步驟 5 來初始化
+            var x = new X() { Y = "Yee" };    // 可以使用 Object Initializer 來初始化物件...
+            // x.Y = "Haha";                  // CS8852 error
+        }
+
+        /// <summary>
+        /// 步驟 1
+        /// </summary>
+        //public class X
+        //{
+        //    public string Y { get; set; }
+        //}
+
+        /// <summary>
+        /// 步驟 2
+        /// </summary>
+        //public class X
+        //{
+        //    public string Y { get; }
+        //}
+
+        /// <summary>
+        /// 步驟 3
+        /// </summary>
+        //public class X
+        //{
+        //    public string Y { get; }
+
+        //    public X(string y)
+        //    {
+        //        Y = y;
+        //    }
+        //}
+
+        /// <summary>
+        /// 步驟 4
+        /// </summary>
+        //public class X
+        //{
+        //    public string Y { get; init; }
+
+        //    public X(string y)
+        //    {
+        //        Y = y;
+        //    }
+        //}
+
+        /// <summary>
+        /// 步驟 5
+        /// </summary>
+        public class X
+        {
+            private readonly string _y;
+
+            public string Y
+            {
+                get => _y;
+                init => _y = (value ?? throw new ArgumentNullException(nameof(Y)));
+            }
         }
     }
 }
